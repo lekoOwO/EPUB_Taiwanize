8aa27f9f342e643955cd780f3d5db2f20c5827e7
showgan <rexx0520@gmail.com>
2020-01-12 22:36:19 +0800

    Update PS Script

--- convert.ps1
+++ convert.ps1
@@ -1,5 +1,10 @@
-Set-Location -Path $args[0]
-Get-ChildItem "." -Filter *.epub |
-Foreach-Object {
-    pythonw $PSScriptRoot/main.py $_.FullName
+if ((Get-Item $args[0]) -is [System.IO.DirectoryInfo]) {
+    Set-Location -Path $args[0]
+    Get-ChildItem "." -Filter *.epub |
+    Foreach-Object {
+        pythonw $PSScriptRoot/main.py $_.FullName
+    }
+}
+else {
+    pythonw $PSScriptRoot/main.py $args[0]
 }
