5bf25f9e0c02f4f8fa547f6e1b2d0cc0abc3e303
Leko <rexx0520@gmail.com>
2021-04-03 12:53:38 +0800

    fix: script file

--- convert.ps1
+++ convert.ps1
@@ -3,7 +3,7 @@ if ((Get-Item $args[0]) -is [System.IO.DirectoryInfo]) {
     Set-Location -Path $args[0]
     Get-ChildItem -Path $args[0] -Filter *.epub |
         Foreach-Object -Parallel {
-            pythonw $using:py $_.FullName
+            pythonw "$using:py" "${_.FullName}"
         } -ThrottleLimit 99999 -AsJob | Receive-Job -Wait
 }
 else {
--- convert.sh
+++ convert.sh
@@ -6,8 +6,8 @@ PYFILE="$SCRIPTPATH/main.py"
 if [ -d "$1" ]; then
   # $1 is a dir
   cd "$1"
-  find . -iname '*.epub' -printf "python $PYFILE '%p' > /dev/null 2>&1 &\0" | xargs -L1 -0 -P0 sh -c
+  find . -iname '*.epub' -printf "python3 $PYFILE '%p' > /dev/null 2>&1 &\0" | xargs -L1 -0 -P0 sh -c
   # find . -type f -name '*.epub' -print0 | parallel -j16 -0 python "$PYFILE" {}
 else
-  python "$PYFILE" "$1" > /dev/null 2>&1 &
+  python3 "$PYFILE" "$1" > /dev/null 2>&1 &
 fi
